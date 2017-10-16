71cdfb42392a10ffef2f56cce622b9c08c4df1f9
abhay-venkatesh <abhay.venkatesh@gmail.com>
2017-10-15 21:16:04 -0500

    Working code

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+from __future__ import division, absolute_import, print_function
+import os, sys, time, re, json
+import numpy as np
+import matplotlib.pyplot as plt
 from unrealcv import client
+
 client.connect()
 if not client.isconnected():
     print('UnrealCV server is not running. Run the game downloaded from http://unrealcv.github.io first.')
     sys.exit(-1)
+else:
+
+	for round in range(1,5):
+		
+
+		client.request('vset /camera/0/location 5 5 300')
+		client.request('vset /camera/0/rotation 5 5 5')
+	  	res = client.request('vget /camera/0/lit ./batch/lit.png')
+		print('The image is saved to %s' % res)
