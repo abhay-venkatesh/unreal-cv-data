141f3e8570a70acda448cf54efe75b3d3c65d223
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-02-08 09:15:06 -0600

    Stop for now

--- scripts/resize_dataset.py
+++ scripts/resize_dataset.py
@@ -4,3 +4,8 @@ Resizes dataset
 1. Resize to 720x450
 2. Converts PNGs to JPEGs
 """
+
+from PIL import Image
+im = Image.open("pic0.png")
+rgb_im = im.convert('RGB')
+rgb_im.save('pic0.jpg', 'JPEG', quality=90)
