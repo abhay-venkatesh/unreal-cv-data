056bb7f2d772c31ea2c34697b5a40def9fa2ab9b
abhay-venkatesh <abhay.venkatesh@gmail.com>
2018-02-08 20:21:06 -0600

    Resize dataset script

--- scripts/resize_dataset.py
+++ scripts/resize_dataset.py
@@ -1,11 +1,28 @@
 """
 Resizes dataset
 
-1. Resize to 720x450
-2. Converts PNGs to JPEGs
+1. Converts PNGs to JPEGs
+2. Renames them appropriately with angles attached
 """
-
 from PIL import Image
-im = Image.open("pic0.png")
-rgb_im = im.convert('RGB')
-rgb_im.save('pic0.jpg', 'JPEG', quality=90)
+import os
+
+# Build output directories
+output_directory = "../../../../UnrealEngineSourceResized/"
+if not os.path.exists(output_directory):
+  os.makedirs(output_directory) 
+
+source_directory = "../../../../UnrealEngineSource/"
+
+# Walk through the DatasetSource and pick samples
+for dirName, subdirList, fileList in os.walk(source_directory):
+  absDirName = os.path.abspath(dirName)
+  newDirName = absDirName.replace("UnrealEngineSource", "UnrealEngineSourceResized")
+  if not os.path.exists(newDirName):
+    os.makedirs(newDirName) 
+
+  for fname in fileList:
+    print(dirName + '\\' + fname)
+    im = Image.open(dirName + '\\' + fname)
+    im = im.convert('RGB')
+    im.save(newDirName + '\\' + fname.replace('png', 'jpg'), 'JPEG', quality=90)
