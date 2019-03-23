827ea5d286be856e83c34d451cd10ce7f7c3f7dd
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-03-23 14:23:32 -0500

    remove checkpoints

--- starter.py
+++ starter.py
@@ -2,7 +2,8 @@ from unrealcv import client
 client.connect()  # Connect to the game
 if not client.isconnected(
 ):  # Check if the connection is successfully established
-    print('UnrealCV server is not running. Run the game from http://unrealcv.github.io first.')
+    print("UnrealCV server is not running. "
+          "Run the game from http://unrealcv.github.io first. ")
 else:
     filename = client.request('vget /camera/0/lit')
     filename = client.request('vget /camera/0/depth depth.exr')
