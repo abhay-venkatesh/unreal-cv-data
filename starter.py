3188f0f9dc9e096151be64075f08f0b6cf53c2bb
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-03-23 17:08:14 -0500

    stop for now

--- starter.py
+++
@@ -1,9 +0,0 @@
-from unrealcv import client
-client.connect()  # Connect to the game
-if not client.isconnected(
-):  # Check if the connection is successfully established
-    print("UnrealCV server is not running. "
-          "Run the game from http://unrealcv.github.io first. ")
-else:
-    filename = client.request('vget /camera/0/lit')
-    filename = client.request('vget /camera/0/depth depth.exr')
