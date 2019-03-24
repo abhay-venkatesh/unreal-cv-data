75d11a1e9b6db037f2ab22b510482b3572875256
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-03-24 09:02:49 -0500

    stop for now

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 from lib.unreal_collector import UnrealCollector
+from unrealcv import client
 
 
 def setup():
-    # Comment the following line out when you are done setting up.
-    # raise NotImplementedError("Please read setup() in main.py")
-
     # First, setup your Unreal Engine 4 environment,
+
+    # Then, setup the config file
+    # client.connect()
+    # if not client.isconnected():
+    #     raise RuntimeError("Could not connect to client. ")
+    # print(client.request("vget /unrealcv/status"))
+    # client.disconnect()
+
     # Then, assign an environment name
     ENVIRONMENT_NAME = "modular_neighborhood"
+
+    # Comment the following line out when you are done setting up.
+    # raise NotImplementedError("Please read setup() in main.py")
 
     return ENVIRONMENT_NAME
 
