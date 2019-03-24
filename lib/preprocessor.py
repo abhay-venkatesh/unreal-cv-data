a5af9b1542f6cbe95ebb2f5a1dfa11037908216d
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-03-24 07:13:50 -0500

    stop for now

--- lib/preprocessor.py
+++ lib/preprocessor.py
@@ -22,12 +22,13 @@ class Color(object):
 
 class PreProcessor:
     def __init__(self, dest_dir):
-        self.dest_dir = dest_dir
+        self.dest_dir = Path(dest_dir, "json")
         self.colors_file = Path(self.dest_dir, "colors.json")
 
     def preprocess(self):
         if not os.path.exists(self.colors_file):
             self._get_colors()
+        self._condense_colors()
 
     def _get_colors(self):
         client.connect()
@@ -37,7 +38,7 @@ class PreProcessor:
         scene_objects = client.request('vget /objects').split(' ')
         print('Number of objects in this scene:', len(scene_objects))
 
-        colors = dict()
+        colors = {}
         for scene_obj in tqdm(scene_objects):
             request_str = "vget /object/" + scene_obj + "/color"
             colors[scene_obj] = client.request(request_str)
@@ -45,3 +46,22 @@ class PreProcessor:
             json.dump(colors, fp)
 
         client.disconnect()
+
+    def _condense_colors(self):
+        with open(self.colors_file) as fp:
+            colors = json.load(fp)
+        condensed_colors = {}
+        scene_obj_to_class = {}
+        for scene_obj, rgba in colors.items():
+            first_item = scene_obj.split("_")[0]
+            if first_item == "Road" or first_item == "Hedge":
+                if first_item not in condensed_colors.keys():
+                    condensed_colors[first_item] = rgba
+                scene_obj_to_class[scene_obj] = first_item
+            else:
+                condensed_colors[scene_obj] = rgba
+
+        with open(Path(self.dest_dir, "condensed_colors.json"), 'w') as fp:
+            json.dump(condensed_colors, fp)
+        with open(Path(self.dest_dir, "scene_obj_to_class.json"), 'w') as fp:
+            json.dump(scene_obj_to_class, fp)
