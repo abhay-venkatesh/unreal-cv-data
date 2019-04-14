669b6c0cb84cd090c687583710ff7c2f5d6f232e
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-14 10:57:08 -0500

    stop for now

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import importlib
+from importlib import import_module as im
 
 if __name__ == "__main__":
     """ Uncomment the tool you are interested in """
 
-    # importlib.import_module("toolbox.collect").collect("modular_neighborhood")
-    importlib.import_module("toolbox.build_default_dataset").build()
+    # im("toolbox.collect").collect("modular_neighborhood")
+    # im("toolbox.build_default_dataset").build()
+    im("toolbox.find_class").find_class(rgba="(R=191,G=0,B=223,A=255)")
--- toolbox/find_class.py
+++ toolbox/find_class.py
@@ -2,8 +2,8 @@ from lib.unreal_collector import UnrealCollector
 from lib.misc import Color
 
 
-def find_class():
-    color = Color("(R=78,G=78,B=111,A=255)")
+def find_class(rgba="(R=191,G=0,B=223,A=255)"):
+    color = Color(rgba)
     uc = UnrealCollector("modular_neighborhood")
     obj_id = uc.get_obj_from_color(color)
     print(obj_id)
