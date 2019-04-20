6cdb9edc95d2bf135f2b25284f4ecc81e33a08f7
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-20 12:13:41 -0500

    fix

--- toolbox/build_testing_dataset.py
+++ toolbox/build_testing_dataset.py
@@ -5,17 +5,20 @@ from unrealcv import client
 
 
 def build():
-    environment_folder = UnrealCollector("testing").environment_folder
+    default_environment_folder = UnrealCollector(
+        "testing_default").environment_folder
 
     client.connect()
     if not client.isconnected():
         raise RuntimeError("Could not connect to client. ")
 
-    builder = Builder(environment_folder)
+    builder = Builder(default_environment_folder)
     points = builder.get_random_points(400)
-
     builder.build_from_points(points)
-    PreProcessor(environment_folder).preprocess()
+
+    new_environment_folder = UnrealCollector("testing_new").environment_folder
+    builder = Builder(new_environment_folder)
+    PreProcessor(new_environment_folder).preprocess()
     builder.build_from_points(points)
 
     client.disconnect()
