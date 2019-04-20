c35436f80f5b383c6f88cd3f3ab35cc30c60c551
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-20 10:59:04 -0500

    indent

--- toolbox/build_testing_dataset.py
+++ toolbox/build_testing_dataset.py
@@ -1,12 +1,20 @@
-from lib.preprocessor import PreProcessor
 from lib.builder import Builder
+from lib.preprocessor import PreProcessor
+from unrealcv import client
 
 
 def build():
     environment_folder = "testing"
+
+    client.connect()
+    if not client.isconnected():
+        raise RuntimeError("Could not connect to client. ")
+
     builder = Builder(environment_folder)
     points = builder.get_random_points(400)
+
     builder.build_from_points(points)
-    preprocessor = PreProcessor(environment_folder)
-    preprocessor.preprocess()
+    PreProcessor(environment_folder).preprocess()
     builder.build_from_points(points)
+
+    client.disconnect()
