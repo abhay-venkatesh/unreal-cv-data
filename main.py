c0486bb6584ace1c80fbfb8fcc1b2ad942e6476a
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2019-04-25 06:58:40 -0500

    fix info

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from importlib import import_module as im
 if __name__ == "__main__":
     """ Uncomment the tool you are interested in """
 
-    im("lib.unreal_collector").UnrealCollector("modular_neighborhood").collect(
-        count=4000)
+    im("lib.unreal_collector").UnrealCollector("neighborhood_stuff").collect(
+        count=400)
     # im("toolbox.build_testing_dataset").build()
     # im("toolbox.build_default_dataset").build()
     # im("toolbox.find_class").find_class(rgba="(R=0,G=78,B=207,A=255)")
